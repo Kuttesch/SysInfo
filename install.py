bb1219bfe71692e31e5a97c920f98ce46ee76618
Kuttesch <106610385+Kuttesch@users.noreply.github.com>
2024-06-06 10:31:54 +0200

    refactored code to maintain readability and expandability

--- install.py
+++ install.py
@@ -5,8 +5,15 @@ import sys
 import re
 import ast
 
-Install_Path = os.path.expanduser('~/SysInfo')
+
 Installer_Path = os.path.abspath('SysInfo.py')
+
+Install_Path_Windows = os.path.expanduser('~/SysInfo')
+Install_Path_Linux = os.path.expanduser('~/.local/bin/SysInfo')
+
+libraries = ['psutil', 'screeninfo', 'py-cpuinfo', 'colorama']
+
+alias = "SysInfo"
 
 # Function to get the version number from a Python script
 def get_version_number(script_path):
@@ -21,47 +28,17 @@ def get_version_number(script_path):
         return None
         print("DEBUG: Version number of {script_path} not found.")
 
-
-# Function to install the SysInfo script
-def install():
-    alias = "SysInfo"
-
-    print("Installing Sysinfo...")
-
-    # Create the folder
-    if os.path.exists(Install_Path):
-        pass
+# Function to check the OS
+def osCheck():
+    if sys.platform == 'win32':
+        return 'Windows'
+    elif sys.platform == 'linux':
+        return 'Linux'
     else:
-        os.makedirs(Install_Path, exist_ok=True)
+        return None
 
-    # Check if the PowerShell profile exists
-    profile_check = subprocess.run(['powershell', '-Command', 'Test-Path $PROFILE'], capture_output=True, text=True)
-    if 'True' in profile_check.stdout:
-        # Check if the alias already exists in the profile
-        alias_check = subprocess.run(['powershell', '-Command', f'if (Get-Command {alias} -ErrorAction SilentlyContinue) {{ "True" }} else {{ "False" }}'], capture_output=True, text=True)
-        if 'True' in alias_check.stdout:
-            print("Alias already exists.")
-        else:
-            command = f'Add-Content -Path $PROFILE -Value "`nfunction {alias} {{ python {Install_Path} }}"'
-            subprocess.run(['powershell', '-Command', command], check=True)
-            print("DEBUG: Alias added to existing profile.")
-    else:
-        # If the profile doesn't exist, ask the user if they want to create it
-        user_input = input("A PowerShell profile does not exist. Do you want to create one? (y/n): ")
-        if user_input.lower() == 'y':
-            subprocess.run(['powershell', '-Command', 'New-Item -path $PROFILE -type file -force'], check=True)
-            command = f'Add-Content -Path $PROFILE -Value "`nfunction {alias} {{ python {Install_Path} }}"'
-            subprocess.run(['powershell', '-Command', command], check=True)
-            print("DEBUG: Profile created and alias added.")
-        else:
-            print("Quitting installation...")
-            sys.exit(0)
-
-
-    # List of necessary libraries
-    libraries = ['psutil', 'screeninfo', 'py-cpuinfo', 'colorama']
-
-    # Install the necessary libraries
+# Function to install the required libraries
+def libInstall():
     for library in libraries:
         try:
             result = subprocess.run([sys.executable, '-m', 'pip', 'install', '--upgrade', library], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
@@ -72,51 +49,91 @@ def install():
         except subprocess.CalledProcessError:
             print(f"Failed to install {library}!")
 
-    # Copy the SystemInfo.py script to the folder
-    shutil.copy(Installer_Path, Install_Path)
+def successMessage():
 
-
-    # Print the success message
     print("Sysinfo has been installed successfully!")
-
     print("Please remove the installer directory manually: " + os.path.dirname(Installer_Path))
-
     # Check if the user wants to restart the terminal
     print("Please restart your terminal for the changes to take effect.")
 
+def quitMessage():
+    print("Quitting installation...")
+    sys.exit(0)
+
+# Function to install the SysInfo script
+def installWindows():
+
+    print("Installing Sysinfo...")
+
+    # Create the folder
+    if os.path.exists(Install_Path_Windows):
+        pass
+    else:
+        os.makedirs(Install_Path_Windows, exist_ok=True)
+
+    # Check if the PowerShell profile exists
+    profile_check = subprocess.run(['powershell', '-Command', 'Test-Path $PROFILE'], capture_output=True, text=True)
+    if 'True' in profile_check.stdout:
+        # Check if the alias already exists in the profile
+        alias_check = subprocess.run(['powershell', '-Command', f'if (Get-Command {alias} -ErrorAction SilentlyContinue) {{ "True" }} else {{ "False" }}'], capture_output=True, text=True)
+        if 'True' in alias_check.stdout:
+            print("Alias already exists.")
+        else:
+            command = f'Add-Content -Path $PROFILE -Value "`nfunction {alias} {{ python {Install_Path_Windows} }}"'
+            subprocess.run(['powershell', '-Command', command], check=True)
+            print("DEBUG: Alias added to existing profile.")
+    else:
+        # If the profile doesn't exist, ask the user if they want to create it
+        user_input = input("A PowerShell profile does not exist. Do you want to create one? (y/n): ")
+        if user_input.lower() == 'y':
+            subprocess.run(['powershell', '-Command', 'New-Item -path $PROFILE -type file -force'], check=True)
+            command = f'Add-Content -Path $PROFILE -Value "`nfunction {alias} {{ python {Install_Path_Windows} }}"'
+            subprocess.run(['powershell', '-Command', command], check=True)
+            print("DEBUG: Profile created and alias added.")
+        else:
+            print("Quitting installation...")
+            sys.exit(0)
+
+    # Install the required libraries
+    libInstall()
+
+    # Copy the SystemInfo.py script to the folder
+    shutil.copy(Installer_Path, Install_Path_Windows)
+
+    successMessage()
 
 # Check if there's already a version of SysInfo at the target destination
-if os.path.exists(Install_Path):
+if os.path.exists(Install_Path_Windows):
     # Get the version numbers
     Installer_Version = get_version_number(Installer_Path)
-    Existing_Version = get_version_number(Install_Path + '/SysInfo.py')
+    Existing_Version = get_version_number(Install_Path_Windows + '/SysInfo.py')
 
     # Compare the version numbers
     if Existing_Version == None or Installer_Version == None or Installer_Version > Existing_Version:
         print("A newer version of SysInfo is available.")
         check = input("Do you want to update SysInfo? (y/n): ")
         if check.lower() == 'y':
-            install()
+            installWindows()
         else:
-            print("Quitting installation...")
-            sys.exit(0)
+            quitMessage()
+            
     elif Installer_Version < Existing_Version:
         print("You have a newer version of SysInfo than the one you're trying to install.")
         check = input("Do you want to downgrade SysInfo? (y/n): ")
         if check.lower() == 'y':
-            install()
+            installWindows()
         else:
-            print("Quitting installation...")
-            sys.exit(0)
+            quitMessage()
     else:
         print("You already have the latest version of SysInfo.")
         check = input("Do you want to reinstall SysInfo? (y/n): ")
         if check.lower() == 'y':
-            install()
+            installWindows()
         else:
-            print("Quitting installation...")
-            sys.exit(0)
+            quitMessage()
 else:
     check = input("Do you want to install SysInfo? (y/n): ")
     if check.lower() == 'y':
-        install()
+        installWindows()
+    else:
+        quitMessage()
